bd10dd63fb71d95a482c523cbb5cebdece86e88f
aitianyu macbook <aitianyu.cn@outlook.com>
2025-08-03 22:21:49 +0800

    fix bug for when error occur in TEST_HOOK
    branch: dev
    user: senyun.yao

--- packages/infra/TestHook.ts
+++ packages/infra/TestHook.ts
@@ -2,6 +2,10 @@
 
 export class TestHook {
     public static debugger(_info?: string): void {
-        __TIANYU_SHELL_TEST_HOOK__?.debugger?.(_info);
+        try {
+            __TIANYU_SHELL_TEST_HOOK__?.debugger?.(_info);
+        } catch {
+            // this is to avoid error in TEST_HOOK
+        }
     }
 }
